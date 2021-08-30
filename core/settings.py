51fab1144f3ba20ec1b52ec515778c3cf0995d7d
Michael Herman <hermanmu@gmail.com>
2021-08-30 14:36:01 -0600

    Update README.md

--- core/settings.py
+++ core/settings.py
@@ -141,6 +141,47 @@ REST_FRAMEWORK = {
 
 ELASTICSEARCH_DSL = {
     'default': {
-        'hosts': 'localhost:9200'
+        'hosts': '146.190.88.115:9200'
     },
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': 'velname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'logstash': {
+            'level': 'WARNING',
+            'class': 'logstash.TCPLogstashHandler',
+            'host': 'localhost',
+            'port': 5959,  # Default value: 5959
+            # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
+            'version': 1,
+            # 'type' field in logstash message. Default value: 'logstash'.
+            'message_type': 'django',
+            # Fully qualified domain name. Default value: false.
+            'fqdn': False,
+            'tags': ['django.request'],  # list of tags. Default: None.
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['logstash'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+    }
 }
